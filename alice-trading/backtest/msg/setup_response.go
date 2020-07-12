6b4d55415634609706fd2b60b3cc059a0496cb0c
fmyaaaaaaa <fmyaaaaaaa@gmail.com>
2020-07-12 13:51:40 +0900

    [ISSUES-7][alice-backtest]バックテストを利用できるように実装する

--- alice-trading/backtest/msg/setup_response.go
+++ alice-trading/backtest/msg/setup_response.go
@@ -4,3 +4,10 @@ type SetupResponse struct {
 	Status int    `json:"status"`
 	Result string `json:"result"`
 }
+
+func NewSetupResponse(status int, result string) SetupResponse {
+	return SetupResponse{
+		Status: status,
+		Result: result,
+	}
+}
