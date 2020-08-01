87c3dc74d052e667d3d8fe7a760dd9d6d883dc74
fmyaaaaaaa <fmyaaaaaaa@gmail.com>
2020-08-01 22:48:25 +0900

    [ISSUES-9][alice-backtest]バックテストの機能性・利便性を向上

--- alice-backtest/bin/make_feed_data.sh
+++ alice-backtest/bin/make_feed_data.sh
@@ -19,7 +19,10 @@ URL_TO_GET_CANDLES="https://api-fxpractice.oanda.com/v3/instruments/$TARGET_PROD
 # カールを実行し結果をcsv形式で格納
 curl -X GET -H "$HEADER_CONTENT_TYPE" -H "$HEADER_AUTH" "$URL_TO_GET_CANDLES" | \
  jq -r '["datetime", "open", "high", "low", "close", "adj close", "volume"], (.candles[]|[.time, .mid["o"], .mid["h"], .mid["l"], .mid["c"], .mid["c"], .volume])|@csv' | \
- sed 's/\"//g' > "$OUTPUT_DIR/candles-$TARGET_PRODUCT-$TARGET_GRANULARITY.csv"
+ sed 's/\"//g' > "$OUTPUT_DIR/$TARGET_PRODUCT-$TARGET_GRANULARITY.csv"
 
-## トレード計画検証用のcsvには以下のコマンドを実行する。（銘柄名、足種は適宜修正が必要。）
-# sed -e "/09:00:00.000000000Z/d" candles-USD_JPY-H12.csv | sed "/10:00:00.000000000Z/d" > candles-USD_JPY-H12_eliminate.csv
+if [ "$TARGET_GRANULARITY" = "H12" ]; then
+    sed -e "/09:00:00.000000000Z/d" "$OUTPUT_DIR/$TARGET_PRODUCT-H12.csv" | sed "/10:00:00.000000000Z/d" > "$OUTPUT_DIR/$TARGET_PRODUCT-H12_el.csv"
+    rm "$OUTPUT_DIR/$TARGET_PRODUCT-H12.csv"
+    mv "$OUTPUT_DIR/$TARGET_PRODUCT-H12_el.csv" "$OUTPUT_DIR/$TARGET_PRODUCT-H12.csv"
+fi
--- alice-backtest/main.py
+++ alice-backtest/main.py
@@ -7,6 +7,8 @@ import marshmallow_dataclass
 import backtrader as bt
 import datetime
 
+import pandas as pd
+
 
 @dataclass
 class Candle:
@@ -97,10 +99,10 @@ class AliceStrategy(bt.Strategy):
         pos_size = self.getposition(self.data0).size
         if pos_size < 0:
             if self.datas[1].close[-1] < float(candle_24.close_price):
-                self.buy(size=pos_size)
+                self.close()
         elif pos_size > 0:
             if self.datas[1].close[-1] > float(candle_24.close_price):
-                self.sell(size=(-1 * pos_size))
+                self.close()
 
         # ----- Start 12-candle Check Trade Plan -----
         req_trade_plan_12 = urllib.request.Request(url_trade_plan, json.dumps(param_candle_12).encode(), headers)
@@ -112,12 +114,12 @@ class AliceStrategy(bt.Strategy):
         if trade_plan_12.get("is_order"):
             if trade_plan_12.get("buy_sell") == 'BUY':
                 if pos_size >= 0:
-                    self.buy(size=100, exectype=bt.Order.StopTrail, trailamount=0.1)
-                    print('BUY CREATE - 12,', candle_24.time, candle_24.close_price)
+                    self.buy(size=1000, exectype=bt.Order.StopLimit, plimit=(float(candle_24.close_price) * 1.003))
+                    print('BUY CREATE - 12,', candle_24.time, candle_24.close_price, pos_size)
             elif trade_plan_12.get("buy_sell") == 'SELL':
                 if pos_size <= 0:
-                    self.sell(size=100, exectype=bt.Order.StopTrail, trailamount=0.1)
-                    print('SELL CREATE - 12,', candle_24.time, candle_24.close_price)
+                    self.sell(size=1000, exectype=bt.Order.StopLimit, plimit=(float(candle_24.close_price) * 1.003))
+                    print('SELL CREATE - 12,', candle_24.time, candle_24.close_price, pos_size)
         # ----- End 12-candle Check Trade Plan -----
 
         # ----- Start 24-candle Check Set Up -----
@@ -135,14 +137,13 @@ class AliceStrategy(bt.Strategy):
         if trade_plan_24.get("is_order"):
             if trade_plan_24.get("buy_sell") == 'BUY':
                 if pos_size >= 0:
-                    self.buy(size=100, exectype=bt.Order.StopTrail, trailamount=0.1)
-                    print('BUY CREATE - 24,', candle_24.time, candle_24.close_price)
+                    self.buy(size=1000, exectype=bt.Order.StopLimit, plimit=(float(candle_24.close_price) * 1.003))
+                    print('BUY CREATE - 24,', candle_24.time, candle_24.close_price, pos_size)
             elif trade_plan_24.get("buy_sell") == 'SELL':
                 if pos_size <= 0:
-                    self.sell(size=100, exectype=bt.Order.StopTrail, trailamount=0.1)
-                    print('SELL CREATE - 24,', candle_24.time, candle_24.close_price)
+                    self.sell(size=1000, exectype=bt.Order.StopLimit, plimit=(float(candle_24.close_price) * 1.003))
+                    print('SELL CREATE - 24,', candle_24.time, candle_24.close_price, pos_size)
         # ----- End 24-candle Check Trade Plan -----
-
         # --- Back Test Area End ---
 
 
@@ -153,9 +154,9 @@ def run_start(args=None):
     # 複数の足種を利用する際は、小さい足種からデータを読み込む必要がある。
     # 12時間足を読み込む。
     data_12hour = bt.feeds.YahooFinanceCSVData(
-        dataname='./data/candles-USD_JPY-H12_eliminate.csv',
-        fromdate=datetime.datetime(2020, 1, 1),
-        todate=datetime.datetime(2020, 7, 6),
+        dataname='./data/' + args.instrument + '-H12.csv',
+        fromdate=datetime.datetime(2016, 1, 1),
+        todate=datetime.datetime(2020, 7, 1),
         timeframe=bt.TimeFrame.Minutes,
         compression=720,
         round=False,
@@ -164,9 +165,9 @@ def run_start(args=None):
 
     # 日足を読み込む。
     data_day = bt.feeds.YahooFinanceCSVData(
-        dataname='./data/candles-USD_JPY-D.csv',
-        fromdate=datetime.datetime(2020, 1, 1),
-        todate=datetime.datetime(2020, 7, 6),
+        dataname='./data/' + args.instrument + '-D.csv',
+        fromdate=datetime.datetime(2016, 1, 1),
+        todate=datetime.datetime(2020, 7, 1),
         timeframe=bt.TimeFrame.Days,
         round=False,
         reverse=False)
@@ -178,8 +179,69 @@ def run_start(args=None):
     cerebro.addobserver(bt.obs.BuySell)
     cerebro.broker.set_coc(True)
 
+    import backtrader.analyzers as btanalyzers
+    cerebro.addanalyzer(btanalyzers.DrawDown, _name='DrawDown')
+    cerebro.addanalyzer(btanalyzers.TradeAnalyzer, _name='TradeAnalyzer')
+    cerebro.addanalyzer(btanalyzers.SQN, _name='SQN')
+
     print('Starting Cash Value: %.2f' % cerebro.broker.getcash())
-    cerebro.run(stdstats=False)
+    thestrats = cerebro.run(stdstats=False)
+
+    # 実行結果
+    startcash = cerebro.broker.getvalue()
+    thestrat = thestrats[0]
+    print('Max. DrawDown[%]      :{0:.2f}'.format(  # 最大下落率
+        thestrat.analyzers.DrawDown.get_analysis().max.drawdown
+    ))
+    print('Trades                :{0}'.format(  # 取引回数
+        thestrat.analyzers.TradeAnalyzer.get_analysis().total.closed
+    ))
+    print('Win Trades            :{0}'.format(  # 勝ち取引回数
+        thestrat.analyzers.TradeAnalyzer.get_analysis().won.total
+    ))
+    print('Win Avg Price         :{0}'.format(  # 勝ち取引平均利益
+        thestrat.analyzers.TradeAnalyzer.get_analysis().won.pnl.average
+    ))
+    print('Lose Avg Price        :{0}'.format(  # 負け取引平均損失
+        thestrat.analyzers.TradeAnalyzer.get_analysis().lost.pnl.average
+    ))
+    win_rate = (  # 勝率
+            thestrat.analyzers.TradeAnalyzer.get_analysis().won.total
+            / thestrat.analyzers.TradeAnalyzer.get_analysis().total.closed
+    )
+    lost_rate = (  # 敗率
+            thestrat.analyzers.TradeAnalyzer.get_analysis().lost.total
+            / thestrat.analyzers.TradeAnalyzer.get_analysis().total.closed
+    )
+    print('Win Rate[%]           :{0:.2f}'.format(  # 勝率=勝ち取引回数÷全取引回数×100
+        win_rate * 100
+    ))
+    print('Lose Rate[%]          :{0:.2f}'.format(  # 敗率=負け取引回数÷全取引回数×100
+        lost_rate * 100
+    ))
+    print('Best Trade[%]         :{0:.2f}'.format(  # 1回の取引での利益の最大値÷所持金×100
+        thestrat.analyzers.TradeAnalyzer.get_analysis().won.pnl.max / startcash * 100
+    ))
+    print('Worst Trade[%]        :{0:.2f}'.format(  # 1回の取引での損失の最大値÷所持金×100
+        thestrat.analyzers.TradeAnalyzer.get_analysis().lost.pnl.max / startcash * 100
+    ))
+    print('Avg. Trade[%]         :{0:.2f}'.format(  # 損益の平均値÷所持金×100
+        thestrat.analyzers.TradeAnalyzer.get_analysis().pnl.net.average / startcash * 100
+    ))
+    print('Max. Trade Duration   :{0}'.format(  # 1回の取引での最長期間
+        pd.Timedelta(minutes=thestrat.analyzers.TradeAnalyzer.get_analysis().len.max)
+    ))
+    print('Avg. Trade Duration   :{0}'.format(  # 1回の取引での平均期間
+        pd.Timedelta(minutes=thestrat.analyzers.TradeAnalyzer.get_analysis().len.average)
+    ))
+    print('Expectancy[%]         :{0:.2f}'.format(  # 期待値=平均利益×勝率＋平均損失×敗率
+        thestrat.analyzers.TradeAnalyzer.get_analysis().won.pnl.average * win_rate
+        + thestrat.analyzers.TradeAnalyzer.get_analysis().lost.pnl.average * lost_rate
+    ))
+    print('SQN                   :{0:.2f}'.format(  # SQN システムの評価値
+        thestrat.analyzers.SQN.get_analysis().sqn
+    ))
+
     print('Final Cash Value: %.2f' % cerebro.broker.getcash())
     cerebro.plot(volume=False)
 
